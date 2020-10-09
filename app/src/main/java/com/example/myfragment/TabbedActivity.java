2f4f9a05ca72ebeda5ffa787d3cb3c9155a9efd1
Shalauddin-bd <mksbd.it@gmail.com>
2020-10-09 11:39:22 +0600

    Redirect to MainActivity on buttonHomepage click done.

--- app/src/main/java/com/example/myfragment/TabbedActivity.java
+++ app/src/main/java/com/example/myfragment/TabbedActivity.java
@@ -1,13 +1,27 @@
 package com.example.myfragment;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TabbedActivity extends AppCompatActivity {
 
+    Button btnHomepage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
+
+        btnHomepage = findViewById(R.id.buttonHomePage);
+
+        btnHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TabbedActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
