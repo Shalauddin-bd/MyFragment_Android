e32dd960b05791b2b101ea438886b4fb8a4767f5
Shalauddin-bd <mksbd.it@gmail.com>
2020-10-13 00:47:00 +0600

    Tabbed activity modified.

--- app/src/main/java/com/example/myfragment/TabbedActivity.java
+++ app/src/main/java/com/example/myfragment/TabbedActivity.java
@@ -4,17 +4,32 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 public class TabbedActivity extends AppCompatActivity {
 
-    Button btnHomepage;
+    TabOneFragment oneFragment;
+    TabTwoFragment twoFragment;
+    Button btnHomepage, btnTabOne, btnTabTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
 
         btnHomepage = findViewById(R.id.buttonHomePage);
+        btnTabOne = findViewById(R.id.buttonTabOne);
+        btnTabTwo = findViewById(R.id.buttonTabTwo);
+
+        oneFragment = new TabOneFragment();
+        twoFragment = new TabTwoFragment();
+
+//        transaction.add( R.id.linearLayoutContainer1, oneFragment,"FirstFragment");
+//        transaction.add( R.id.linearLayoutContainer2, twoFragment,"SecondFragment");
+//        transaction.commit();
+
 
         btnHomepage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +38,27 @@ public class TabbedActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnTabOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager   manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add( R.id.linearLayoutContainerTabbedActivity, oneFragment,"FirstFragment");
+                transaction.commit();
+            }
+        });
+
+        btnTabTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager   manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add( R.id.linearLayoutContainerTabbedActivity, twoFragment,"SecondFragment");
+                transaction.commit();
+            }
+        });
+
+
     }
 }
